7584aceb4d0561016f17708c97a5dae67a0f51f2
dseflinger <dseflinger@gmail.com>
2025-06-02 14:23:57 -0700

    added some interfaces for special cases

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,24 @@ import { DndContext } from '@dnd-kit/core'
 import Draggable from './Draggable'
 import Droppable from './Droppable'
 
+export enum PhraseType {
+  default,
+  multibox,
+  lightsaber
+}
+
+export interface BasePhrase {
+  text: string;
+  type?: PhraseType;
+}
+
+export interface MultiGiftPhrase extends BasePhrase {
+  boxcount: number;
+  type: PhraseType.multibox;
+}
+
+type Phrase = BasePhrase | MultiGiftPhrase;
+
 function App() {
   const phraseRef = useRef<HTMLDivElement>(null)
   const giftRef = useRef<HTMLImageElement>(null)
@@ -20,28 +38,28 @@ function App() {
   const [decoysClicked, setDecoysClicked] = useState(new Set());
   const [parent, setParent] = useState(null);
 
-  // todo maybe add isSpecial or enum type
-  const phrases = [
-    "Open your gift for a special suprise!",
-    "Definitely not best man material",
-    "Bottom of leaderboard",
-    "Peaked in High School",
-    "One twin is definitely uglier",
-    "Your mom definitely has a favorite",
-    "Its a little birthday present, get it?",
-    "What happened?",
-    "Midlife crisis starts in 3… 2…",
-    "Open it and reveal your final form (disappointment)",
-    "Noah raw-dogs ketchup packets",
-    "Matthew eats a bowl of Cheetos with cheese",
-    "WHICH ONE IS IT??? CHOOSE WISELY",
-    "HOW ABOUT NOW???",
-    "Why?",
-    "Disney adults",
-    "They mark Hispanic on on forms but immediately ask where the mayo is",
-    "You're closer to 40 now than to 20",
-    "They were supposed to be triplets, but one clearly ate the third",
-    "KILL THE YOUNGLING, DO IT, DO IT NOW"
+  const phrases: Phrase[] = [
+    { text: "Open your gift for a special suprise!" },
+    { text: "Definitely not best man material" },
+    { text: "Bottom of leaderboard" },
+    { text: "Peaked in High School" },
+    { text: "One twin is definitely uglier" },
+    { text: "Your mom definitely has a favorite" },
+    { text: "Its a little birthday present, get it?" },
+    { text: "What happened?" },
+    { text: "Midlife crisis starts in 3… 2…" },
+    { text: "Open it and reveal your final form (disappointment)" },
+    { text: "Noah raw-dogs ketchup packets" },
+    { text: "Matthew eats a bowl of Cheetos with cheese" },
+    { text: "WHICH ONE IS IT??? CHOOSE WISELY", type: PhraseType.multibox, boxcount: 4 },
+    { text: "HOW ABOUT NOW???", type: PhraseType.multibox, boxcount: 20 },
+    { text: "Why?" },
+    { text: "Disney adults" },
+    { text: "They mark Hispanic on on forms but immediately ask where the mayo is" },
+    { text: "You're closer to 40 now than to 20" },
+    { text: "They were supposed to be triplets, but one clearly ate the third" },
+    { text: "KILL THE YOUNGLING, DO IT, DO IT NOW", type: PhraseType.lightsaber },
+    { text: "one more just to test" },
   ]
 
   useEffect(() => {
@@ -99,20 +117,29 @@ function App() {
     const gift = giftRef.current;
     const middleFinger = middleFingerRef.current;
     const phrase = phraseRef.current;
-    if (!gift || !middleFinger || !phrase) {
+    if (!middleFinger || !phrase) {
       return;
     }
+    setDecoysClicked(new Set());
     setClickCount(prev => prev + 1);
-    setDecoysClicked(new Set())
     console.log(`clickcount ${clickCount}`)
     if (clickCount < 30) {
       moveGift();
     } else {
-      gift.style.display = 'none';
+      if (gift)
+        gift.style.display = 'none';
       phrase.style.display = 'none';
       middleFinger.classList.remove('hidden');
       triggerConfetti();
     }
+  }
+
+  function getGiftSizeClass(boxCount: number) {
+    if (boxCount <= 4) return 'w-32 h-32';
+    if (boxCount <= 8) return 'w-24 h-24';
+    if (boxCount <= 12) return 'w-20 h-20';
+    if (boxCount <= 20) return 'w-16 h-16';
+    return 'w-12 h-12';
   }
 
   const handleDecoy = (index: number) => {
@@ -125,73 +152,70 @@ function App() {
 
 
   function handleDragEnd({ over }: any) {
-    setParent(over ? over.id : null);
+    if (over) {
+      setParent(over.id);
+      handleClick();
+    }
   }
+
+  const renderContent = () => {
+    switch (phrases[clickCount].type) {
+      case PhraseType.multibox:
+        var phrase = phrases[clickCount] as MultiGiftPhrase;
+        var boxcount = phrase.boxcount;
+        const sizeClass = getGiftSizeClass(boxcount);
+        // todo fix randomIndex (its always been 0 and 3)
+        return (
+          <div className='flex space-between flex-wrap justify-center  mt-16'>
+            {Array.from({ length: boxcount }, (_, index) => index).map((_, i) => (
+              <img
+                key={`${clickCount}-${i}`}
+                src={decoysClicked.has(i) ? chosePoorlyImage : giftImage}
+                className={`animate-bounce transition-all scale-100 ${sizeClass}`}
+                onClick={i === randomIndex ? handleClick : () => handleDecoy(i)}
+              />
+            ))}
+          </div>
+        );
+      case PhraseType.lightsaber:
+        return (
+          <DndContext onDragEnd={handleDragEnd}>
+            <div>
+              {!parent ? (
+                <Draggable id="draggable-item">
+                  <img src={lightsaberImage} alt="Lightsaber" className="w-32" />
+                </Draggable>
+              ) : null}
+              <Droppable id="drop-zone">
+                <img src={younglingImage} alt="Youngling" className="w-32" />
+              </Droppable>
+            </div>
+          </DndContext>
+        );
+      default:
+        return (
+          <div className='flex space-between flex-wrap justify-center mt-16'>
+            <img
+              id="gift"
+              ref={giftRef} src={giftImage}
+              onClick={handleClick}
+              className={`animate-bounce invisble transition-all duration-300 ease-in-out scale-100 ${clickCount == 0 || clickCount == 6 ? '' : 'absolute'} ${clickCount === 6 ? 'w-8 h-8' : 'w-32 h-32'}`} />
+          </div>
+        );
+    }
+  };
 
   return (
     <div>
       <div ref={phraseRef} className='flex flex-col items-center'>
-        <span className='text-2xl md:text-3xl lg:text-4xl'>{phrases[clickCount]}</span>
+        <span className='text-2xl md:text-3xl lg:text-4xl'>{phrases[clickCount].text}</span>
       </div>
-      {clickCount !== 12 && clickCount != 13 && (
-        <div className='flex space-between flex-wrap justify-center mt-16'>
-          <img
-            id="gift"
-            ref={giftRef} src={giftImage}
-            onClick={handleClick}
-            className={`animate-bounce invisble transition-all duration-300 ease-in-out scale-100 ${clickCount == 0 || clickCount == 6 ? '' : 'absolute'} ${clickCount === 6 ? 'w-8 h-8' : 'w-32 h-32'}`} />
-        </div>
-      )}
+
       <div ref={middleFingerRef} className='flex flex-col items-center hidden'>
         <span className='text-2xl md:text-3xl lg:text-4xl font-bold mb-8'>Happy 30th Birthday Fuckers!</span>
         <img id="middle-finger" src={middleFingerImage} className='w-64' />
       </div>
-      {clickCount === 12 && (
-        <div className='flex space-between flex-wrap justify-center  mt-16'>
-          {[0, 1, 2, 3].map((_, i) => (
-            <img
-              key={i}
-              ref={giftRef}
-              src={decoysClicked.has(i) ? chosePoorlyImage : giftImage}
-              className="animate-bounce transition-all duration-300 ease-in-out scale-100 w-32 h-32"
-              onClick={i === randomIndex ? handleClick : () => handleDecoy(i)}
-            />
-          ))}
-        </div>
-      )}
-      {clickCount === 13 && (
-        <div className='flex space-between flex-wrap justify-center  mt-16'>
-          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19].map((_, i) => (
-            <img
-              key={i}
-              ref={giftRef}
-              src={decoysClicked.has(i) ? chosePoorlyImage : giftImage}
-              className="animate-bounce transition-all duration-300 ease-in-out scale-100 w-16 h-16"
-              onClick={i === randomIndex20 ? handleClick : () => handleDecoy(i)}
-            />
-          ))}
-        </div>
-      )}
-      {clickCount === 3 && (
-        <div>
-          <DndContext onDragEnd={handleDragEnd}>
-            {!parent ? (
-              <Draggable id="draggable-item">
-                <img src={lightsaberImage} alt="Lightsaber" className="w-16 h-16" />
-              </Draggable>
-            ) : null}
-            <Droppable id="drop-zone">
-              {parent === 'drop-zone' ? (
-                <img src={younglingImage} alt="Youngling" className="w-24 h-24" />
-              ) : (
-                <div className="w-24 h-24 border-2 border-dashed border-gray-400 flex items-center justify-center text-sm">
-                  Drop here
-                </div>
-              )}
-            </Droppable>
-          </DndContext>
-        </div>
-      )}
+      {renderContent()}
     </div>
   )
 }
