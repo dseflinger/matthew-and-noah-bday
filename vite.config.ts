149c159983f627448949984808599447585e65ad
dseflinger <dseflinger@gmail.com>
2025-06-13 14:33:24 -0700

    added custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/matthew-and-noah-bday/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
