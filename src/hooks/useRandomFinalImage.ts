a112cc3fe1b47cdae9fec0c46af0be6b7d434e30
dseflinger <dseflinger@gmail.com>
2025-06-13 20:01:30 -0700

    added craig gif

--- src/hooks/useRandomFinalImage.ts
+++ src/hooks/useRandomFinalImage.ts
@@ -2,8 +2,9 @@ import { useCallback, useState } from "react";
 import colbert from '../assets/colbert_middle.gif'
 import ryan from '../assets/ryan_flip.gif'
 import rodgers from '../assets/mrrogers_flip.webp'
+import craig from '../assets/craig_flip.gif'
 
-const choices = [colbert, ryan, rodgers];
+const choices = [colbert, ryan, rodgers, craig];
 
 const randomFinalImage = () => {
     const randomIndex = Math.floor(Math.random() * choices.length);
